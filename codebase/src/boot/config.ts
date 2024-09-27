293a2fa2b181a40bcf9d2c48881f074f71dc5037
mliebaut <melusine.liebaut@gmail.com>
2024-09-27 10:15:41 +0200

    clean repository

--- codebase/src/boot/config.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-import path from "path";
-
-process.env.TZ = "Etc/Universal";
-const envPath = process.env.NODE_ENV === "test" ? ".env.test" : ".env";
-dotenv.config({path: path.resolve(__dirname, "..", "..", envPath)});
